878be6074a5de6455463d892eb81887f12a7f543
NamPhunPhin <Shagara258@gmail.com>
2023-05-11 19:41:14 +0700

    Update fix FooterComponent.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,17 @@
 <template>
-<<<<<<< HEAD
-  <NavbarComponent />
-  <!-- <img src="/images/banner.png" alt="" /> -->
-</template>
-
-<script>
-import NavbarComponent from "./components/NavbarComponent.vue";
-export default {
-  name: "App",
-  components: { NavbarComponent },
-=======
   <div>
-    <FooterComponent></FooterComponent>
+    <NavbarComponent/>
+    <FooterComponent/>
   </div>
 </template>
 
 <script>
+import NavbarComponent from './components/NavbarComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   name: "App",
-  components: {FooterComponent},
->>>>>>> e723f27c7fd85c43c58ad734d28efb1cbf1cc7e1
+  components: {NavbarComponent, FooterComponent},
 };
 </script>
 
